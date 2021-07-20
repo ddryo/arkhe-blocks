340cb21f8f866e92077d0f8fe0a35508c050d2e3
ddryo <dd.bocsfcryo@gmail.com>
2021-07-20 16:11:12 +0900

    設定追加

--- inc/setup.php
+++ inc/setup.php
@@ -8,7 +8,14 @@ add_action( 'init', function() {
 	\Arkhe::set_plugin_data( 'use_arkhe_blocks', true );
 } );
 
-
 add_action( 'after_setup_theme', function() {
-	add_theme_support( 'custom-spacing' );
-}, 11 );
+	if ( \Arkhe_Blocks::get_data( 'general', 'use_custom_space' ) ) {
+		add_theme_support( 'custom-spacing' );
+	}
+	if ( \Arkhe_Blocks::get_data( 'general', 'use_fse_blocks' ) ) {
+		add_filter( 'arkhe_use_fse_blocks', '__return_true' );
+	}
+	if ( \Arkhe_Blocks::get_data( 'general', 'use_core_patterns' ) ) {
+		add_theme_support( 'core-block-patterns' );
+	}
+}, 20 );
